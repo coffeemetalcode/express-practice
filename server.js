4bdcb0e46bda566448f44b7714cff3ff9dd95a83
dbstocker <dstocker@notesettersinc.com>
2018-12-30 14:05:40 -0500

    a basic express server with a single route

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
+// use Express.js
 var express = require("express");
 
+// set express to variable name 'app'
 var app = express();
 
+// set PORT to PORT on the server or 3017 in localhost
 var PORT = process.env.PORT || 3017;
 
+// what to do when a user goes to the root page
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-app.listen(PORT, () => {
+// start the server and display a message in console to show the server is up
+var server = app.listen(PORT, () => {
   console.log(`App is listening on port ${PORT}!`);
 });
